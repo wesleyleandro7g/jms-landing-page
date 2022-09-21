183e50ec311c85892fdf764edf725ea91f0c810c
Wesley Leandro <wesleyleandro.dev@gmail.com>
2022-09-21 13:05:10 -0300

    Created a footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,22 @@
+import React from "react";
+
+import styles from "../styles/footer.module.css";
+
 export function Footer(): JSX.Element {
   return (
-    <footer>
-      <h1>roda pé</h1>
+    <footer className={styles.footer}>
+      <h1>James Moto Shop</h1>
+
+      <div>
+        <a href="#" target="_blank">
+          Termos de uso
+        </a>
+        <a href="#" target="_blank">
+          Políticas de privacidade
+        </a>
+      </div>
+
+      <h6>Copyright 2022 © James Moto Shop</h6>
     </footer>
   );
 }
