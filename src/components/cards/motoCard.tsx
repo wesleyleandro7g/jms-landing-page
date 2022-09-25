944a9dfb741056fb846944c76038285170d5bf62
Wesley Leandro <wesleyleandro.dev@gmail.com>
2022-09-25 17:14:24 -0300

    Updated motoCard style

--- src/components/cards/motoCard.tsx
+++ src/components/cards/motoCard.tsx
@@ -4,16 +4,30 @@ import biz from "../../../public/images/motorcycles/BIZ_110/BIZ_110_BRANCA_LATER
 
 import { PrimaryButton } from "../button";
 
-import styles from "../../styles/motorcycleCard.module.css";
-
 export function MotoCard() {
   return (
-    <div className={styles.container}>
-      <h1>Biz 110i</h1>
+    <div
+      className="
+        flex
+        flex-col
+        justify-center
+        w-full
+        min-w-[320px]
+        max-w-[540px]
+        md:min-w-[220px]
+        md:max-w-[450px]
+        p-2
+        gap-4
+        md:gap-2
+        rounded-lg
+        bg-white
+      "
+    >
+      <h1 className="text-lg font-semibold text-black-800">Biz 110i</h1>
       <Image src={biz} layout="responsive" />
       <div>
-        <p>Parcelas mensais a partir de</p>
-        <h5>R$ 155,48</h5>
+        <p className="text-sm text-black-800">Parcelas mensais a partir de</p>
+        <h5 className="text-3xl text-black-800 font-semibold">R$ 155,48</h5>
       </div>
       <PrimaryButton
         style={{
