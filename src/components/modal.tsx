c41f93da859f460a403610891589b1c646f92e90
Wesley Leandro <wesleyleandro.dev@gmail.com>
2022-09-25 18:28:21 -0300

    Updated modal component css

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,7 +1,5 @@
 import React, { forwardRef, useCallback, useImperativeHandle } from "react";
 
-import styles from "../styles/modal.module.css";
-
 export interface ModalHandles {
   toggleModal: () => void;
 }
@@ -33,11 +31,57 @@ const Modal: React.ForwardRefRenderFunction<ModalHandles, ModalProps> = (
   });
 
   return (
-    <div id="modalFilter" className={styles.overlay}>
-      <div className={styles.content}>{props.children}</div>
-      <button className={styles.close} onClick={toggleModal}>
+    <div
+      id="modalFilter"
+      className="
+        hidden
+        justify-center
+        items-end
+        absolute
+        bottom-0
+        right-0
+        w-full
+        h-full
+        z-[999]
+        border-none
+        bg-black/75
+        backdrop-blur-sm
+      "
+    >
+      <div
+        className="
+          flex
+          flex-col
+          justify-center
+          items-center
+          w-full
+          rounded-t-2xl
+          p-4
+          gap-12
+          bg-white
+          z-[9999]
+        "
+      >
+        {props.children}
+      </div>
+      <button className="absolute" onClick={toggleModal}>
         Fechar
       </button>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="absolute z-[9999] top-5 right-5 w-10 h-10 text-white"
+        viewBox="0 0 512 512"
+      >
+        <title>Close</title>
+        <path
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="32"
+          d="M368 368L144 144M368 144L144 368"
+        />
+      </svg>
     </div>
   );
 };
